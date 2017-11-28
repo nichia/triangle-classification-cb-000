676c301bc76e19a12f1226d4dc6420de2cd2b920
Ni Chia <nichia@gmail.com>
2017-11-28 18:11:38 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
   # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+    
+  if invalid_triangle
+    begin
+      raise TriangleError
+    rescue TriangleError => error
+      puts error.message
+    end
+  else
+  end
+end
+
+class TriangleError < StandardError
+  def message
+    "triangles violating triangle inequality are illegal"
+  end
 end
