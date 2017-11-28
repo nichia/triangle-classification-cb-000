7b8b286548ddb826d13df2ff0ebaeb9862ddbce2
Ni Chia <nichia@gmail.com>
2017-11-28 18:20:58 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,14 +7,25 @@ class Triangle
     @side2 = side2
     @side3 = side3
   end
-    
-  if invalid_triangle
-    begin
-      raise TriangleError
-    rescue TriangleError => error
-      puts error.message
-    end
-  else
+
+  def kind
+    if side1 <= 0 || side2 <= 0 || side3 <= 0 ||  
+      side1 + side2 <= side3 || 
+      side1 + side3 <= side2 || 
+      side2 + side3 <= side1
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
+    else
+      if side1 == side2 && side2 == side3
+        :equilateral
+      elsif side1 == side2 || side1 == side3 || side2 == side3
+        :isosceles
+      else
+        :scalene    
+      end
   end
 end
 
